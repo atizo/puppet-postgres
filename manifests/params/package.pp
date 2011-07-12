4dddc69323daa5700a1a43bc997456cf62e39699
Simon Josi <simon.josi@atizo.com>
2011-07-12 20:35:45 +0200

    fix package names

--- manifests/params/package.pp
+++ manifests/params/package.pp
@@ -1,5 +1,5 @@
 class postgres::params::package {
   $client = "postgresql$postgres::params::package_suffix"
-  $server = "postgresql-server$postgres::params::package_suffix"
-  $devel = "postgresql-devel$postgres::params::package_suffix"
+  $server = "postgresql${postgres::params::package_suffix}-server"
+  $devel = "postgresql${postgres::params::package_suffix}-devel"
 }
