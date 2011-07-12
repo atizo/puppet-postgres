0ae0ef0bb244ae11e7d4ed59722667e602b12248
Simon Josi <simon.josi@atizo.com>
2011-07-12 20:57:25 +0200

    fix dep

--- manifests/server.pp
+++ manifests/server.pp
@@ -18,6 +18,7 @@ class postgres::server {
     creates => "$postgres::params::datapath_base/data/postgresql.conf",
     require => Package['postgresql-server'],
     before => [
+      Service['postgresql-server'],
       File["$postgres::params::datapath_base/data/pg_hba.conf"],
       File["$postgres::params::datapath_base/data/postgresql.conf"],
     ],
