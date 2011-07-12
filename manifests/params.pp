e77ecd081e17d0047982088efe7eb488822ef8c5
Simon Josi <simon.josi@atizo.com>
2011-07-12 20:45:02 +0200

    add missing slash in path

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class postgres::params{
   if $postgres::version {
     $package_suffix = regsubst($postgres::version, '\.','', 'G')
     $service_suffix = "-$postgres::version"
-    $datapath_suffix = "$postgres::version"
+    $datapath_suffix = "/$postgres::version"
   }
   $datapath_base = "/var/lib/pgsql$datapath_suffix"
   $service = "postgresql$service_suffix"
